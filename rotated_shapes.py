1f5e9f581b920814c153c8c0c83085343da6e2e2
akiskowitz <iskowan@ssfs.org>
2022-09-14 09:56:23 -0400

    Commit_Save_3

--- rotated_shapes.py
+++ rotated_shapes.py
@@ -1,13 +1,18 @@
 import turtle
 
 turtle.speed(500)
-def makeASquare():
-    for x in range(20):
-        turtle.rt(20)
-        for x in range(4):
-            turtle.rt(90)
-            turtle.fd(100)
+# def makeASquare():
+    # for x in range(20):
+        # turtle.rt(20)
+        # for x in range(4):
+            # turtle.rt(90)
+            # turtle.fd(100)
+
+turtle.rt(270)
+def shapeAngle():
+    for x in range(1):
+        turtle.fd(50)
+        turtle.rt(55)
 
 
-makeASquare()
 turtle.exitonclick()
