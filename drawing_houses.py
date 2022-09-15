d3e0d93a3afe90a92c1d26cbb1331a7152569f42
akiskowitz <iskowan@ssfs.org>
2022-09-15 11:51:46 -0400

    Commit_Save_9

--- drawing_houses.py
+++ drawing_houses.py
@@ -3,29 +3,49 @@ import turtle
 # defining turtle
 t = turtle.Turtle()
 
-def makeASquare(color):
+# This makes the house
+def makeASquare(color, size):
     t.color(color)
     t.begin_fill()
     for x in range(4):
-        t.fd(100)
+        t.fd(size)
         t.rt(90)
     t.end_fill()
 
-def makeATriangle(color):
+# This makes roof
+def makeATriangle(color, size):
     t.color(color)
     t.begin_fill()
     t.rt(-60)
-    t.fd(100)
-    t.rt(120)
-    t.fd(100)
-    t.rt(120)
-    t.fd(100)
+    for x in range(3):
+        t.fd(size)
+        t.rt(120)
     t.end_fill()
 
+# this combines them to make it one definition
+def makeHouse(color, size, color2, size2):
+    makeASquare(color, size)
+    makeATriangle(color2, size2)
 
-t.hideturtle()
 
-makeASquare("blue")
-makeATriangle("red")
+
+t.speed(100)
+#this makes a house and states the colors
+makeHouse("blue", 100, "red", 100)
+t.penup()
+t.goto(-100, -50)
+t.lt(300)
+t.pendown()
+makeHouse("green", 50, "yellow", 50)
+t.penup()
+t.goto(-175,-25)
+t.lt(300)
+t.pendown()
+makeHouse("pink", 75, "orange", 75)
+t.penup()
+t.goto(175, -0)
+t.lt(300)
+t.pendown()
+makeHouse("purple", 100, "green", 100)
 
 turtle.exitonclick()
